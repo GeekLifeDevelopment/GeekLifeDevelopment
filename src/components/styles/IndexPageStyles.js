aa9150419ee11fdf37caacf98c3030339455102d
Jason Sorenson <jasonsorenson@Jasons-MacBook-Pro.local>
2019-12-16 22:55:14 -0600

    added hero image

--- src/components/styles/IndexPageStyles.js
+++ src/components/styles/IndexPageStyles.js
@@ -1,5 +1,21 @@
 import styled from "styled-components"
 
 export const IndexPageStyles = styled.div`
-  color: green;
+  #hero-image-container {
+    position: relative;
+    #overlay {
+      position: absolute;
+      top: 0;
+      background: rgba(0, 0, 0, 0.7);
+      width: 100%;
+      height: 100vh;
+    }
+    #image-container {
+      img {
+        width: 100vw;
+        height: 100vh;
+        object-fit: cover;
+      }
+    }
+  }
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import HeroImage from "../images/geek-life-development-hero-image.jpg"
 
 import Layout from "../components/Layout"
 import LatestBlog from "../components/LatestBlogs"
@@ -10,7 +11,13 @@ const IndexPage = () => {
     <div>
       <Layout>
         <IndexPageStyles>
-          <div id="hero-image-container"></div>
+          <div id="hero-image-container">
+            <div id="overlay"></div>
+            <div id="image-container">
+              <img src={HeroImage} alt="" />
+            </div>
+            <div id="hero-overlay-container"></div>
+          </div>
 
           <LatestBlog />
         </IndexPageStyles>
