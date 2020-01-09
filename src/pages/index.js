e349b9cd7b33ef7269062111c3db4daf3dc5b4a1
Jason Sorenson <jasonsorenson@Jasons-MacBook-Pro.local>
2020-01-08 20:45:47 -0600

    fixed goofy side scroll

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => {
             </div>
           </div>
 
-          <LatestBlog />
+          {/* <LatestBlog /> */}
         </IndexPageStyles>
       </Layout>
     </div>
