e5e1475eb309a40bcdf5bccecdb5f4bdd9e1961e
Jason Sorenson <jasonsorenson@Jasons-MacBook-Pro.local>
2020-01-30 21:15:17 -0600

    fixed responsize blogs

--- src/components/styles/LatestBlogStyles.js
+++ src/components/styles/LatestBlogStyles.js
@@ -4,9 +4,13 @@ export const LatestBlogStyles = styled.div`
   ol {
     background-color: #88928c;
     display: flex;
-    flex-wrap: wrap;
+    /* flex-wrap: wrap; */
     justify-content: space-around;
     transform: skewY(-5deg);
+
+    @media (max-width: 1220px) {
+      flex-direction: column;
+    }
 
     li {
       padding: 1.5rem;
@@ -23,9 +27,13 @@ export const LatestBlogStyles = styled.div`
     box-shadow: 5px 5px 10px #333;
     overflow: hidden;
 
+    @media (max-width: 1220px) {
+      width: 100%;
+    }
+
     #category-label {
       position: absolute;
-      font-size: 1.2em;
+      font-size: 1.3em;
       right: -80px;
       color: white;
       width: 300px;
@@ -56,7 +64,7 @@ export const LatestBlogStyles = styled.div`
 
         h1 {
           text-align: center;
-          font-size: 2em;
+          font-size: 5vh;
           margin: 0;
           text-shadow: 5px 5px 5px #333;
         }
