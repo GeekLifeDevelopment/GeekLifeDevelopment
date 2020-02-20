eda01172c8a7ec7cddc6ee9050b4470fbf3bafd9
Jason Sorenson <jasonsorenson@Jasons-MacBook-Pro.local>
2020-02-19 19:05:16 -0600

    messing with product card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,16 @@ import { ProjectCardStyle } from "./styles/ProjectCardStyles"
 const ProjectCard = props => {
   return (
     <ProjectCardStyle>
-      <div id="overlay"></div>
+      <div id="overlay">
+        <div className="overlay-content">
+          <h1>Butcher & Barrel</h1>
+          <p>
+            Simple resturant website using Bootstrap, HTML5 and a little
+            Javascript
+          </p>
+          <button>More...</button>
+        </div>
+      </div>
       <div id="card-container">
         <img src={props.image} alt="" />
       </div>
--- src/components/styles/ProjectCardStyles.js
+++ src/components/styles/ProjectCardStyles.js
@@ -22,5 +22,31 @@ export const ProjectCardStyle = styled.div`
     background-color: rgba(0, 0, 0, 0.5);
     border-radius: 10%;
     z-index: 2;
+    /* display: none; */
+
+    &:hover {
+      cursor: pointer;
+      display: block;
+    }
+  }
+
+  .overlay-content {
+    margin-top: 20%;
+    text-align: center;
+    color: white;
+    transition-duration: 0.3s;
+
+    h1 {
+      font-size: 1.5em;
+    }
+    button {
+      width: 100px;
+      font-size: 1em;
+      padding: 0.3rem;
+      color: white;
+      background-color: #83b799;
+
+      border: none;
+    }
   }
 `
