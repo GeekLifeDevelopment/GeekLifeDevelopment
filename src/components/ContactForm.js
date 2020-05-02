cbc2671b0edcbd0744d39892b6f0a34542b0b617
Jason Sorenson <jasonsorenson@Jasons-MacBook-Pro.local>
2020-05-02 11:48:31 -0500

    finished Contact Me Form component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,12 +2,24 @@ import React from "react"
 import styled from "styled-components"
 
 const FormStyles = styled.div`
+  form {
+    width: 70%;
+    margin-right: auto;
+    margin-left: auto;
+    @media (max-width: 800px) {
+      width: 100%;
+    }
+  }
   input {
     width: 100%;
+    margin-left: auto;
+    margin-right: auto;
     border: none;
     margin-bottom: 1rem;
-    padding: 0.5rem;
+    padding: 0.3rem;
     border-radius: 5px;
+    background-color: #799e9f;
+    color: white;
   }
   textarea {
     width: 100%;
@@ -15,6 +27,18 @@ const FormStyles = styled.div`
     padding: 0.5rem;
     border: none;
     border-radius: 5px;
+    background-color: #799e9f;
+    color: white;
+  }
+
+  button {
+    padding: 2px;
+    border-radius: 10px;
+    border: none;
+    width: 8rem;
+    margin: 0.3rem;
+    background-color: #83b799;
+    color: white;
   }
 `
 
@@ -28,6 +52,7 @@ const ContactForm = () => {
         <input type="email" name="email" placeholder="Email" />
         <textarea name="message" placeholder="What's on your mind?" />
         <button type="submit">Submit</button>
+        <button type="reset">Wait I Goofed!</button>
       </form>
     </FormStyles>
   )
