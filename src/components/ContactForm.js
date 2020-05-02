7d52e0e4d6bae89c6202fcb463f38bbbaab8c721
Jason Sorenson <jasonsorenson@Jasons-MacBook-Pro.local>
2020-05-01 19:50:47 -0500

    creating form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -4,9 +4,16 @@ import styled from "styled-components"
 const FormStyles = styled.div`
   input {
     width: 100%;
+    border: none;
+    margin-bottom: 1rem;
+    padding: 0.5rem;
+    border-radius: 5px;
   }
   textarea {
     width: 100%;
+    height: 200px;
+    border: none;
+    border-radius: 5px;
   }
 `
 
@@ -17,6 +24,7 @@ const ContactForm = () => {
         <input type="text" placeholder="Name" />
         <input type="email" placeholder="Email" />
         <textarea />
+        <button type="submit">Submit</button>
       </form>
     </FormStyles>
   )
