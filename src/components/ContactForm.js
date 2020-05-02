1644e31ee9be491d8243f71fc2e62c8a5fc6a10e
Jason Sorenson <jasonsorenson@Jasons-MacBook-Pro.local>
2020-05-01 21:14:49 -0500

    test orm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -20,7 +20,7 @@ const FormStyles = styled.div`
 const ContactForm = () => {
   return (
     <FormStyles>
-      <form>
+      <form data-netlify="true">
         <input type="text" name="name" placeholder="Name" />
         <input type="email" placeholder="Email" />
         <textarea />
