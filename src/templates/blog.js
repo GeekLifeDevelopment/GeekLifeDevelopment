90184a7b98af74a479ddfc81b5d7fa3f88e528f5
Geek Life Development <jason.sorenson@geeklifedevelopment.com>
2020-05-15 12:01:36 -0500

    Update blog.js

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,16 +2,40 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Img from "gatsby-image"
+import styled from 'styled-components'
+
+
+const BlogBody = styled.div`
+  width: 80%;
+  padding: .5rem;
+  margin: 3rem auto;
+
+h1 {
+text-align: center;
+  margin: 2rem 0;
+  padding: 3rem 0;
+  font-weight: bold;
+  border-bottom: 4px solid #83b799;
+  width: 700px;
+  transform: rotate(-5deg);
+@media (max-width: 750px) {
+  width: 350px;
+margin: 2rem auto;
+}
+}
+`
+
 export default ({ data }) => {
   let post = data.markdownRemark
   let featuredImgFluid = post.frontmatter.featuredImage.childImageSharp.fluid
   return (
     <Layout>
-      <div style={{width: '80%', padding: '.5rem', margin: 'auto'}}>
-        <h1>{post.frontmatter.title}</h1>
+      <BlogBody>
+        
         <Img fluid={featuredImgFluid} />
+    <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
+      </BlogBody>
     </Layout>
   )
 }
