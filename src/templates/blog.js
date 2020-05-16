2b7b6da9603c6feb32888c438366dbe1340edf5c
Jason Sorenson <jasonsorenson@Jasons-MacBook-Pro.local>
2020-05-15 21:24:38 -0500

    fixed mobile titles in blog posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,28 +2,27 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Img from "gatsby-image"
-import styled from 'styled-components'
-
+import styled from "styled-components"
 
 const BlogBody = styled.div`
   width: 80%;
-  padding: .5rem;
+  padding: 0.5rem;
   margin: 3rem auto;
 
-h1 {
-  text-align: center;
-  margin: 2rem 0;
-  padding: 1rem 0;
-  font-weight: bold;
-  border-bottom: 4px solid #83b799;
-  width: 700px;
-  transform: rotate(-5deg);
-  
-  @media (max-width: 750px) {
-    width: 350px;
+  h1 {
+    text-align: center;
     margin: 2rem auto;
+    padding: 1rem 0;
+    font-weight: bold;
+    border-bottom: 4px solid #83b799;
+    width: 700px;
+    /* transform: rotate(-5deg); */
+
+    @media (max-width: 750px) {
+      width: 300px;
+      margin: 2rem auto;
+    }
   }
-}
 `
 
 export default ({ data }) => {
@@ -31,12 +30,14 @@ export default ({ data }) => {
   let featuredImgFluid = post.frontmatter.featuredImage.childImageSharp.fluid
   return (
     <Layout>
-    <div id="image-container" style={{width: '100%', height: '450px'}}>
-    <Img fluid={featuredImgFluid} style={{width: '100%', height: '100%', objectFit: 'cover'}}/>
-    </div>
+      <div id="image-container" style={{ width: "100%", height: "450px" }}>
+        <Img
+          fluid={featuredImgFluid}
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+      </div>
       <BlogBody>
-        
-    <h1>{post.frontmatter.title}</h1>
+        <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </BlogBody>
     </Layout>
